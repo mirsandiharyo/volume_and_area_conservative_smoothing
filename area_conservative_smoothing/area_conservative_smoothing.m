c51af89b30e0a27e1e4e4fa0e29881e6da5c54dd
mirsandiharyo <mirsandiharyo@gmail.com>
2020-07-20 23:16:17 +0200

    Added a function to read the grid

---
+++ area_conservative_smoothing/area_conservative_smoothing.m
@@ -0,0 +1,20 @@
+%% 
+% Area conserving smoothing of curve grids using single node 
+% or two nodes relaxations.
+% Reference: Kuprat et al., J Comput. Phys., 172 (2001), 99-118.
+% Created by: Haryo Mirsandi
+
+close all;
+clearvars;
+mkdir output;
+delete output/iter_*;
+
+%% set the smoothing parameter and method
+max_iter = 50;
+two_nodes = true; % if false then single node relaxation is used.
+
+%% read the grid
+[marker, point] = read_grid();
+
+
+
---
+++ area_conservative_smoothing/read_grid.m
@@ -0,0 +1,38 @@
+% Read the grid from the input file. 
+% Each marker element consists of two points (vertices). 
+% The points are stored in counter-clockwise direction for consistent
+% calculation of normal vector.
+function[marker, point] = read_grid()
+    origin_path = pwd;
+    disp('choose the input file (.txt)');
+    [input_name, file_path] = uigetfile('.txt');
+    cd(file_path);
+    fid = fopen(input_name);
+    % read the marker elements
+    read_line = regexp(fgetl(fid), '\t', 'split');
+    marker.total = str2double(read_line{2});
+    [marker.vertex1, marker.vertex2] = deal(zeros(marker.total, 1));
+    fgetl(fid);
+    % each marker has two vertices
+    for num=1:marker.total
+        read_line = regexp(fgetl(fid), '\t', 'split');
+        marker_num = str2double(read_line{1});
+        marker.vertex1(marker_num) = str2double(read_line{2});
+        marker.vertex2(marker_num) = str2double(read_line{3});    
+    end
+    fgetl(fid);
+    % read the point coordinates
+    read_line = regexp(fgetl(fid), '\t', 'split');
+    point.total = str2double(read_line{2});
+    point.coord = zeros(point.total, 2);
+    fgetl(fid);
+    for num=1:point.total
+        read_line = regexp(fgetl(fid), '\t', 'split');
+        point_num = str2double(read_line{1});
+        % x and y coordinates
+        point.coord(point_num, 1) = str2double(read_line{2});
+        point.coord(point_num, 2) = str2double(read_line{3});    
+    end
+    fclose(fid);
+    cd(origin_path);
+end
