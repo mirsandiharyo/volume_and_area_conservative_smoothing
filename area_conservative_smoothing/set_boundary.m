f09462de81ac2d12d421f825ae70e1ecdf592731
mirsandiharyo <mirsandiharyo@gmail.com>
2020-07-23 17:47:32 +0200

    Cleaning

--- area_conservative_smoothing/set_boundary.m
+++ area_conservative_smoothing/set_boundary.m
@@ -1,13 +1,14 @@
-% set the boundary for visualization
+% Set the boundary for visualization.
 function[xmin, xmax, ymin, ymax] = set_boundary(point)
+    factor = 0.1;
     xmin = min(point.coord(:, 1));
     xmax = max(point.coord(:, 1));
     dist_x = xmax-xmin;
     ymin = min(point.coord(:, 2));
     ymax = max(point.coord(:, 2));
     dist_y = ymax-ymin;
-    xmin = xmin-(0.1*dist_x);
-    xmax = xmax+(0.1*dist_x);
-    ymin = ymin-(0.1*dist_y);
-    ymax = ymax+(0.1*dist_y);
+    xmin = xmin-(factor*dist_x);
+    xmax = xmax+(factor*dist_x);
+    ymin = ymin-(factor*dist_y);
+    ymax = ymax+(factor*dist_y);
 end
